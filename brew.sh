1f91a97010dd468a07a26186ff6ef3deed216ecb
Todd Tilby <t.tilby@liverez.com>
2017-01-24 09:14:34 -0700

    add brew.sh

---
+++ brew.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install tools / apps via Homebrew
+
+brew update
+brew upgrade
+
+# Update shell
+brew install bash
+
+# Update built-in tools on mac os
+brew install vim
+brew install git
